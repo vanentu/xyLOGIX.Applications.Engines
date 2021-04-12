78a303828c8bc0b1d96fae50b8f52f5c66f9181f
Brian Hart <brian.hart@xylogix.net>
2021-04-12 17:11:52 -0400

    Deleted parser libraries

--- xyLOGIX.Applications.Engines.Parsers.Models.Interfaces/ICommandLineInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace xyLOGIX.Applications.Engines.Parsers.Models.Interfaces
-{
-    /// <summary>
-    /// Defines the publicly-exposed methods and properties of a POCO whose
-    /// properties are set by parsing command line arguments.
-    /// </summary>
-    public interface ICommandLineInfo { }
-}
