6aacc534089c048580c67c399d4717447c839de7
Brian Hart <brian.hart@xylogix.net>
2021-04-13 11:06:12 -0400

    Update exception message.

--- xyLOGIX.Applications.Engines.Factories/GetApplicationEngine.cs
+++ xyLOGIX.Applications.Engines.Factories/GetApplicationEngine.cs
@@ -5,53 +5,42 @@ using xyLOGIX.Applications.Engines.Interfaces;
 namespace xyLOGIX.Applications.Engines.Factories
 {
     /// <summary>
-    /// Obtains references to instances of objects that implement the
-    /// <see
-    ///     cref="T:xyLOGIX.Applications.Engines.Interfaces.IApplicationEngine" />
+    /// Obtains references to instances of objects that implement the <see
+    /// cref="T:xyLOGIX.Applications.Engines.Interfaces.IApplicationEngine"/>
     /// interface and are of the concrete types specified.
     /// </summary>
     public static class GetApplicationEngine
     {
         /// <summary>
-        /// Creates a new instance of an object that implements the
-        /// <see
-        ///     cref="T:xyLOGIX.Applications.Engines.Interfaces.IApplicationEngine" />
+        /// Creates a new instance of an object that implements the <see
+        /// cref="T:xyLOGIX.Applications.Engines.Interfaces.IApplicationEngine"/>
         /// interface and returns a reference to it.
         /// </summary>
         /// <typeparam name="T">
         /// Name of a concrete type -- and in, or referable by -- the caller's
-        /// own layer of the application, that implements the
-        /// <see
-        ///     cref="T:xyLOGIX.Applications.Engines.Interfaces.IApplicationEngine" />
-        /// interface and corresponds to the specified
-        /// <see
-        ///     cref="T:xyLOGIX.Applications.Engines.Constants.EngineType" />
-        /// value.
+        /// own layer of the application, that implements the <see
+        /// cref="T:xyLOGIX.Applications.Engines.Interfaces.IApplicationEngine"/>
+        /// interface and corresponds to the specified <see
+        /// cref="T:xyLOGIX.Applications.Engines.Constants.EngineType"/> value.
         /// </typeparam>
         /// <param name="type">
-        /// One of the
-        /// <see
-        ///     cref="T:xyLOGIX.Applications.Engines.Constants.EngineType" />
-        /// values
+        /// One of the <see
+        /// cref="T:xyLOGIX.Applications.Engines.Constants.EngineType"/> values
         /// that specifies which type of application engine to create. The type
         /// of engine must correspond to this method's type parameter.
         /// </param>
         /// <returns>
-        /// Reference to an instance of an object that implements the
-        /// <see
-        ///     cref="T:xyLOGIX.Applications.Engines.Interfaces.IApplicationEngine" />
-        /// interface.
+        /// Reference to an instance of an object that implements the <see
+        /// cref="T:xyLOGIX.Applications.Engines.Interfaces.IApplicationEngine"/> interface.
         /// </returns>
         /// <exception cref="T:System.ArgumentOutOfRangeException">
-        /// Thrown if the specified
-        /// <see
-        ///     cref="T:xyLOGIX.Applications.Engines.Constants.EngineType" />
-        /// is not supported.
+        /// Thrown if the specified <see
+        /// cref="T:xyLOGIX.Applications.Engines.Constants.EngineType"/> is not supported.
         /// </exception>
         /// <exception cref="T:System.InvalidOperationException">
-        /// Thrown if the type parameter, <typeparamref name="T" />, is not
+        /// Thrown if the type parameter, <typeparamref name="T"/>, is not
         /// derived from one of the abstract base classes that correspond to the
-        /// <see cref="T:xyLOGIX.Applications.Engines.Constants.EngineType" />
+        /// <see cref="T:xyLOGIX.Applications.Engines.Constants.EngineType"/>
         /// being requested.
         /// </exception>
         public static IApplicationEngine For<T>(EngineType type)
@@ -100,7 +89,7 @@ namespace xyLOGIX.Applications.Engines.Factories
 
                 default:
                     throw new ArgumentOutOfRangeException(
-                        nameof(type), type, null
+                        nameof(type), type, "The type of application engine requested is not supported."
                     );
             }
 
@@ -108,27 +97,21 @@ namespace xyLOGIX.Applications.Engines.Factories
         }
 
         /// <summary>
-        /// Determines whether the object having the type specified by
-        /// <paramref
-        ///     name="potentialDescendantType" />
-        /// is of a class that inherits from,
-        /// or is, the <paramref name="potentialBaseType" />.
+        /// Determines whether the object having the type specified by <paramref
+        /// name="potentialDescendantType"/> is of a class that inherits from,
+        /// or is, the <paramref name="potentialBaseType"/>.
         /// </summary>
         /// <param name="potentialDescendantType">
-        /// A <see cref="T:System.Type" /> that corresponds to the potential
+        /// A <see cref="T:System.Type"/> that corresponds to the potential
         /// child class you are testing.
         /// </param>
         /// <param name="potentialBaseType">
-        /// A <see cref="T:System.Type" /> that corresponds to the base class or interface.
+        /// A <see cref="T:System.Type"/> that corresponds to the base class or interface.
         /// </param>
         /// <returns>
-        /// <see langword="true" /> if the
-        /// <paramref
-        ///     name="potentialDescendantType" />
-        /// inherits or is the
-        /// <paramref
-        ///     name="potentialBaseType" />
-        /// ; <see langword="false" /> otherwise.
+        /// <see langword="true"/> if the <paramref
+        /// name="potentialDescendantType"/> inherits or is the <paramref
+        /// name="potentialBaseType"/> ; <see langword="false"/> otherwise.
         /// </returns>
         private static bool IsDerivedFrom(this Type potentialDescendantType,
             Type potentialBaseType)
